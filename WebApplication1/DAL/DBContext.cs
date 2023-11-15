77d22059a93638e86a38cabe20c504485b7f843a
Gabriel G <gabriel.gassner@outlook.com>
2023-11-14 22:30:04 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/DAL/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.DAL
+{
+    public class DBContext : DbContext
+    {
+
+        public DBContext() : base("DBContext")
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+        }
+    }
+}
